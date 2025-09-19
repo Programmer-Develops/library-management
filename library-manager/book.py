862a7d9083fe5adf836b1ff911f3a2c48bc97124
Programmer-Develops <shachipandya29@gmail.com>
2025-09-19 10:41:27 +0530

    book class setup

---
+++ library-manager/book.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self,title,author,isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.status = 'available'  # can be 'available' or 'borrowed'
+    
+    def __str__(self):
+        return f'"{self.title}" by {self.author} (ISBN: {self.isbn}) - Status: {self.status}'
+    
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "isbn": self.isbn,
+            "status": self.status
+        }
+    
+    def issue(self):
+        if not self.is_available():
+            self.status = 'issued'
+            return True
+        return False
+    
+    def return_book(self):
+        if self.is_available():
+            self.status = 'available'
+            return True
+        return False
+    
+    def is_available(self):
+        return self.status == 'available'
